7852dc586355ff12a26fe7f57fa093939e60c257
Adelev <kak-dela1488@ukr.net>
2023-11-02 12:20:18 +0200

    imageUrlFix

--- src/components/LeftSideBar/LeftSideBar.jsx
+++ src/components/LeftSideBar/LeftSideBar.jsx
@@ -12,15 +12,15 @@ const LeftSideBar = () => {
             <div className='playerName'>{playerName}</div>
             <div className='foodInfo'>
                 <div className='foodBlock'>
-                    <img src='/assets/apple2.png' alt='apple' />
+                    <img src='/assets/apple2-a6754422.png' alt='apple' />
                     <p> – 1 point</p>
                 </div>
                 <div className='foodBlock'>
-                    <img src='/assets/pineapple2.png' alt='apple' />
+                    <img src='/assets/pineapple2-f1ab8d12.png' alt='apple' />
                     <p> – 5 points</p>
                 </div>
                 <div className='foodBlock'>
-                    <img src='/assets/watermelon.png' alt='apple' />
+                    <img src='/assets/watermelon-0aa5a3e9.png' alt='apple' />
                     <p> – 10 points</p>
                 </div>
             </div>
